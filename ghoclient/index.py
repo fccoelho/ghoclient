4bd4d9edce1f5d9d44813bb5f921616b5732ad0d
Flávio Codeço Coelho <fccoelho@gmail.com>
2026-03-14 13:11:34 -0300

    fixed bug when index does not exist

--- ghoclient/index.py
+++ ghoclient/index.py
@@ -1,6 +1,6 @@
-from whoosh.index import create_in, open_dir
+from whoosh.index import create_in, open_dir, exists_in
 from whoosh.qparser import QueryParser
-from whoosh.fields import * 
+from whoosh.fields import *
 import os
 import copy
 
@@ -8,28 +8,30 @@ schema = Schema(code=ID(stored=True), description=TEXT(stored=True))
 
 
 class Index:
-    def __init__(self, index_path='indexdir'):
+    def __init__(self, index_path="indexdir"):
+        self.index_path = index_path
         if not os.path.exists(index_path):
             os.mkdir(index_path)
             self.ix = None
-        else:
+        elif exists_in(index_path):
             self.ix = open_dir(index_path)
-        
+        else:
+            self.ix = None
+
     def build_index(self, codes):
         if self.ix is None:
-            self.ix = create_in("indexdir", schema)
-            writer = self.ix.writer()
-            for row in codes.itertuples():
-                writer.add_document(code=row.Label, description=row.Display)
-            writer.commit()
-            
-        
+            self.ix = create_in(self.index_path, schema)
+        writer = self.ix.writer()
+        for row in codes.itertuples():
+            writer.add_document(code=row.Label, description=row.Display)
+        writer.commit()
+
     def search(self, query):
+        if self.ix is None:
+            return []
         with self.ix.searcher() as searcher:
             qp = QueryParser("description", schema=self.ix.schema)
             q = qp.parse(query)
             results = searcher.search(q, limit=10000)
-            out  = [dict(h) for h in results]
+            out = [dict(h) for h in results]
         return out
-        
-        
